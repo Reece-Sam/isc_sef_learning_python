83c656f6a99246cfa4aad803d55043490dde0440
Reece-Sam <bonisamkelekoh@gmail.com>
2026-03-12 15:41:24 +0100

    study arguments and iterables

--- arguments.py
+++ arguments.py
@@ -35,12 +35,68 @@
 
 #===========================================================
 #Exercise 
-def get_phone(country, area, first, last):
-    return f"{country}-{area}-{first}-{last}"
+# def get_phone(country, area, first, last):
+#     return f"{country}-{area}-{first}-{last}"
 
-phone_num = get_phone(country=237, area=67, first=560, last=3495)
+# phone_num = get_phone(country=237, area=67, first=560, last=3495)
 
-print(phone_num)
+# print(phone_num)
 
 #=====================================================================
 #Arbitrary Arguments
+
+# def add(a, b):
+#     return a + b 
+
+# print(add(1, 2))
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ")
+
+# display_name("Dr.", "Bonisam", "kelekoh", "Namme", "III")
+
+#kwargs 
+# def print_address(**kwargs):
+#     # print(type(kwargs))   #dictionary
+#     for key, value in kwargs.items():
+#         print(f"{key}:{value}")
+
+# print_address(street="Fomic-street", 
+#               apt="207",
+#               town="Buea" , 
+#               division="Fako", 
+#               zip="0000")
+#===================================================================
+#EXERCISE
+
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    print()
+    # for value in kwargs.values():
+    #     print(value, end=" ")
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')}")
+        print(f"{kwargs.get('pobox')}")
+    else:
+        print(f"{kwargs.get('street')}")
+
+    print(f"{kwargs.get('town')} {kwargs.get('division')}, {kwargs.get('zip')}")
+
+shipping_label("Dr.", "Bonisam", "longpants", "IV",
+               street="Fomic-street",
+               pobox="PO box 207",
+               town="Buea" , 
+               division="Fako", 
+               zip="0000")
---
+++ iterables.py
@@ -0,0 +1,32 @@
+#list
+# numbers = [1, 2, 3, 4, 5]
+
+# for number in reversed(numbers):
+#     print(number, end='-')
+
+
+#turple
+# numbers = (1, 2, 3, 4, 5)
+
+# for number in numbers:
+#     print(number, end='-')
+
+#SET
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+#String
+
+# name = "Bonisam"
+
+# for character in name:
+#     print(character, end=" ")
+
+
+#Dictionary
+my_dictionary = {"A":1, "B":2, "C":3}
+
+for key, value in my_dictionary.items():
+    print(f"{key} : {value}")
