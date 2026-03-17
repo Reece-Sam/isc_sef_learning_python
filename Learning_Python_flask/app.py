ae7a9c6ae52c027f69acf4b5ec5f4b3a5e4f3386
Reece-Sam <bonisamkelekoh@gmail.com>
2026-03-17 17:27:59 +0100

    chores:To do list

--- Learning_Python_flask/app.py
+++ Learning_Python_flask/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datebase.db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
 
@@ -19,9 +19,51 @@ class MyTask(db.Model):
         return f"Task (self.id)"
 
 
-@app.route('/')
+@app.route('/', methods= ["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try: 
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"Error:{e}")
+            return f"Error{e}"
+        
+    else: 
+        task = MyTask.query.order_by(MyTask.created).all()
+    return render_template("index.html", tasks=task)
+
+# Delete an item
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"Error:{e}"
+    
+
+#Update an item
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Error{e}"
+    else:
+        return render_template('update.html', task=task)
+
+
+
 
 if __name__ == "__main__":
     with app.app_context():
